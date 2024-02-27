8b4f416b787eda6af7a05472a24d1b87b9f5c9c5
tangoyankee <miller.tim108@gmail.com>
2024-02-27 12:24:24 -0500

    integrate deck gl

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,14 +1,41 @@
 import { Box, Button } from "@nycplanning/streetscape";
+import DeckGL from '@deck.gl/react/typed';
+import { Map } from 'react-map-gl/maplibre';
+import "maplibre-gl/dist/maplibre-gl.css";
+import { ClientOnly } from "remix-utils/client-only";
+
+const INITIAL_VIEW_STATE = {
+  longitude: -74.0008,
+  latitude: 40.7018,
+  zoom: 10,
+  bearing: 0,
+  pitch: 0,
+}
 
 export default function Index() {
   return (
-    <Box bg="gray.700" w="100%" p={4} color="white">
-      <Button
-        variant="secondary"
-        size="sm"
-      >
-        Hello Streetscape!
-      </Button>
-    </Box>
+    <>
+      <Box bg="gray.700" w="100%" p={4} color="white" position="absolute" zIndex={2}>
+        <Button
+          variant="secondary"
+          size="sm"
+        >
+          Hello Streetscape!
+        </Button>
+      </Box>
+      <ClientOnly fallback={<h2>Loading map...</h2>}>
+        {() => 
+          <DeckGL
+            initialViewState={INITIAL_VIEW_STATE}
+            controller={true}
+            style={{ height: "100vh", width: "100vw" }}
+          >
+            <Map
+              mapStyle={"https://tiles.planninglabs.nyc/styles/positron/style.json"}
+            ></Map>
+          </DeckGL>
+        }
+      </ClientOnly>
+    </>
   );
 }
