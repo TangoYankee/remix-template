2d7b291a8a15360ad014389fcc594e6de5aeceab
tangoyankee <miller.tim108@gmail.com>
2024-02-27 10:52:58 -0500

    v2 remix with chakra

---
+++ app/root.tsx
@@ -0,0 +1,76 @@
+import { ChakraProvider, Box, Heading } from "@chakra-ui/react";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+function Document({
+  children,
+  title = "App title",
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <title>{title}</title>
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  // throw new Error("💣💥 Booooom");
+
+  return (
+    <Document>
+      <ChakraProvider>
+        <Outlet />
+      </ChakraProvider>
+    </Document>
+  );
+}
+
+// How ChakraProvider should be used on CatchBoundary
+export function CatchBoundary() {
+
+  return (
+    <Document>
+      <ChakraProvider>
+        <Box>
+          <Heading as="h1" bg="purple.600">
+            [CatchBoundary]: 
+          </Heading>
+        </Box>
+      </ChakraProvider>
+    </Document>
+  );
+}
+
+// How ChakraProvider should be used on ErrorBoundary
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <Document title="Error!">
+      <ChakraProvider>
+        <Box>
+          <Heading as="h1" bg="blue.500">
+            [ErrorBoundary]: There was an error: {error.message}
+          </Heading>
+        </Box>
+      </ChakraProvider>
+    </Document>
+  );
+}
